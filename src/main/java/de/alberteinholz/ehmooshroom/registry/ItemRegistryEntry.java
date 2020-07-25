5885854686fabf2a09f2dd6a15400772054eb947
Albert-Einholz <64534803+Albert-Einholz@users.noreply.github.com>
2020-07-25 00:38:47 -0400

    futher things

--- src/main/java/de/alberteinholz/ehmooshroom/registry/ItemRegistryEntry.java
+++ src/main/java/de/alberteinholz/ehmooshroom/registry/ItemRegistryEntry.java
@@ -2,13 +2,14 @@ package de.alberteinholz.ehmooshroom.registry;
 
 import de.alberteinholz.ehmooshroom.MooshroomLib;
 import net.minecraft.item.Item;
+import net.minecraft.item.Item.Settings;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ItemRegistryEntry {
     private Identifier id;
     //supplied:
-    public Item item; //Should Item.Settings be used instead?
+    public Item item;
 
     protected ItemRegistryEntry(Identifier id) throws NullPointerException {
         if (id == null) {
@@ -17,6 +18,10 @@ public class ItemRegistryEntry {
             throw e;
         }
         this.id = id;
+    }
+
+    public ItemRegistryEntry withItemFactory(ItemFactory<? extends Item> factory, Settings settings) {
+        return withItem(factory.create(settings));
     }
 
     public ItemRegistryEntry withItem(Item item) {
@@ -35,5 +40,10 @@ public class ItemRegistryEntry {
             return;
         }
         if (item != null) Registry.register(Registry.ITEM, id, item);
+    }
+
+    @FunctionalInterface
+    public static interface ItemFactory<I extends Item> {
+        I create(Settings itemSettings);
     }
 }
