891283138cdff59b07b6e9a02fda06b2a24e892f
Albert-Einholz <64534803+Albert-Einholz@users.noreply.github.com>
2021-08-19 13:30:23 -0400

    inventory rewrite final phase fixing a crash

--- src/main/java/de/alberteinholz/ehmooshroom/container/AdvancedContainerBlockEntity.java
+++ src/main/java/de/alberteinholz/ehmooshroom/container/AdvancedContainerBlockEntity.java
@@ -117,13 +117,13 @@ public abstract class AdvancedContainerBlockEntity extends BlockEntity implement
                 BlockComponentHook hook = BlockComponentHook.INSTANCE;
                 if (getConfigComp().allowsConfig(id, ConfigBehavior.SELF_INPUT, dir)) {
                     if (comp instanceof InventoryComponent && hook.hasInvComponent(world, targetPos, targetDir)) comp.pull(hook.getInvComponent(world, targetPos, targetDir), dir, ActionType.PERFORM);
-                    if (comp instanceof TankComponent && hook.hasTankComponent(world, targetPos, targetDir)) comp.pull(hook.getInvComponent(world, targetPos, targetDir), dir, ActionType.PERFORM);
-                    if (comp instanceof CapacitorComponent && hook.hasCapComponent(world, targetPos, targetDir)) comp.pull(hook.getInvComponent(world, targetPos, targetDir), dir, ActionType.PERFORM);
+                    if (comp instanceof TankComponent && hook.hasTankComponent(world, targetPos, targetDir)) comp.pull(hook.getTankComponent(world, targetPos, targetDir), dir, ActionType.PERFORM);
+                    if (comp instanceof CapacitorComponent && hook.hasCapComponent(world, targetPos, targetDir)) comp.pull(hook.getCapComponent(world, targetPos, targetDir), dir, ActionType.PERFORM);
                 }
                 if (getConfigComp().allowsConfig(id, ConfigBehavior.SELF_OUTPUT, dir)) {
                     if (comp instanceof InventoryComponent && hook.hasInvComponent(world, targetPos, targetDir)) comp.push(hook.getInvComponent(world, targetPos, targetDir), dir, ActionType.PERFORM);
-                    if (comp instanceof TankComponent && hook.hasTankComponent(world, targetPos, targetDir)) comp.push(hook.getInvComponent(world, targetPos, targetDir), dir, ActionType.PERFORM);
-                    if (comp instanceof CapacitorComponent && hook.hasCapComponent(world, targetPos, targetDir)) comp.push(hook.getInvComponent(world, targetPos, targetDir), dir, ActionType.PERFORM);
+                    if (comp instanceof TankComponent && hook.hasTankComponent(world, targetPos, targetDir)) comp.push(hook.getTankComponent(world, targetPos, targetDir), dir, ActionType.PERFORM);
+                    if (comp instanceof CapacitorComponent && hook.hasCapComponent(world, targetPos, targetDir)) comp.push(hook.getCapComponent(world, targetPos, targetDir), dir, ActionType.PERFORM);
                 }
             }
         }
