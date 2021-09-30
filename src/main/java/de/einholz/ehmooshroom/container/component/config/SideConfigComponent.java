549bab2bcaf3d7bf5bda396d0e044596e4fe446a
Albert-Einholz <64534803+Albert-Einholz@users.noreply.github.com>
2021-09-30 17:07:27 -0400

    SideConfigComponent rewrite

--- src/main/java/de/einholz/ehmooshroom/container/component/config/SideConfigComponent.java
+++ src/main/java/de/einholz/ehmooshroom/container/component/config/SideConfigComponent.java
@@ -1,5 +1,7 @@
 package de.einholz.ehmooshroom.container.component.config;
 
+import java.util.Set;
+
 import de.einholz.ehmooshroom.MooshroomLib;
 import de.einholz.ehmooshroom.container.component.util.CustomComponent;
 import dev.onyxstudios.cca.api.v3.component.ComponentKey;
@@ -10,9 +12,35 @@ import net.minecraft.util.math.Direction;
 public interface SideConfigComponent extends CustomComponent {
     public static final Identifier SIDE_CONFIG_ID = MooshroomLib.HELPER.makeId("heat");
     public static final ComponentKey<SideConfigComponent> ENERGY = ComponentRegistry.getOrCreate(SIDE_CONFIG_ID, SideConfigComponent.class);
+    public static char[][] DEFAULT_CHARS = getDefault();
 
     //IMPL
-    SideConfigState getState(Identifier id, Direction dir, SideConfigBehavior behavior);
+    char getState(Identifier id, Direction dir, SideConfigBehavior behavior);
+    void setState(Identifier id, Direction dir, SideConfigBehavior behavior, char state);
+
+    //dir first, behavior second
+    static char[][] getDefault() {
+        char[][] ret = new char[6][4];
+        for (Direction dir : Direction.values()) for (SideConfigBehavior behavior : SideConfigBehavior.values()) ret[dir.ordinal()][behavior.ordinal()] = behavior.getDefaultChar();
+        return ret;
+    }
+
+    static String configArrayToString(char[][] config) {
+        String str = "";
+        for (char[] dirConfig : config) for (char c : dirConfig) str = str + c;
+        return str;
+    }
+
+    static char[][] configStringToArray(String config) {
+        char[] configArray = config.toCharArray();
+        char[][] array = new char[6][4];
+        int i = 0;
+        for (Direction dir : Direction.values()) for (SideConfigBehavior behavior : SideConfigBehavior.values()) {
+            array[dir.ordinal()][behavior.ordinal()] = configArray[i];
+            i++;
+        }
+        return array;
+    }
 
     //API
     void addSideConfig(Identifier id);
@@ -21,31 +49,33 @@ public interface SideConfigComponent extends CustomComponent {
     //intersection mode
     void setAvailability(Identifier id, Direction dir, SideConfigBehavior behavior);
     boolean isAvailable(Identifier id, Direction dir,  SideConfigBehavior behavior);
+    void change(Identifier id, Direction dir,  SideConfigBehavior behavior);
 
     default boolean allows(Identifier id, Direction dir, SideConfigBehavior behavior) {
-        return SideConfigState.AVAILABLE_TRUE.equals(getState(id, dir, behavior));
+        return 'T' == getState(id, dir, behavior);
     }
+
+    Set<Identifier> getIds();
 
     @Override
     default Identifier getId() {
         return SIDE_CONFIG_ID;
     }
 
+    //AVAILABLE_TRUE -> T
+    //AVAILABLE_FALSE -> F
+    //RESTRICTED_TRUE -> t
+    //RESTRICTED_FALSE -> f
     public static enum SideConfigBehavior {
-        SELF_INPUT(SideConfigState.AVAILABLE_FALSE),
-        SELF_OUTPUT(SideConfigState.AVAILABLE_FALSE),
-        FOREIGN_INPUT(SideConfigState.AVAILABLE_TRUE),
-        FOREIGN_OUTPUT(SideConfigState.AVAILABLE_TRUE);
+        SELF_INPUT('F'),
+        SELF_OUTPUT('F'),
+        FOREIGN_INPUT('T'),
+        FOREIGN_OUTPUT('T');
 
-        private final SideConfigState def;
+        private final char def;
 
-        private SideConfigBehavior(SideConfigState def) {
+        private SideConfigBehavior(char def) {
             this.def = def;
-        }
-
-        public SideConfigState getForChar(char c) {
-            for (SideConfigState state : SideConfigState.values()) if (c == state.getChar()) return state;
-            return def;
         }
 
         public boolean isDefaultChar(char c) {
@@ -53,37 +83,7 @@ public interface SideConfigComponent extends CustomComponent {
         }
 
         public char getDefaultChar() {
-            return def.getChar();
+            return def;
         }
-    }
-
-    public static enum SideConfigState {
-        AVAILABLE_TRUE('T'),
-        AVAILABLE_FALSE('F'),
-        RESTRICTED_TRUE('t'),
-        RESTRICTED_FALSE('f');
-
-        private final char c;
-
-        private SideConfigState(char c) {
-            this.c = c;
-        }
-
-        public char getChar() {
-            return c;
-        }
-
-        /*XXX
-        public ConfigState getAvailability() {
-
-        }
-
-        public ConfigState setAvailability(boolean available) {
-            char cTemp = available ? Character.toLowerCase(c) : Character.toUpperCase(c);
-            for (ConfigState state : ConfigState.values()) if (cTemp == state.c) return state;
-            MooshroomLib.LOGGER.smallBug(new EnumConstantNotPresentException(ConfigState.class, Character.toString(cTemp)));
-            return this;
-        }
-        */
     }
 }
--- src/main/java/de/einholz/ehmooshroom/container/component/config/SimpleSideConfigComponent.java
+++ src/main/java/de/einholz/ehmooshroom/container/component/config/SimpleSideConfigComponent.java
@@ -2,16 +2,79 @@ package de.einholz.ehmooshroom.container.component.config;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
 
+import net.fabricmc.fabric.api.util.NbtType;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Direction;
 
 public class SimpleSideConfigComponent implements SideConfigComponent {
-    protected final Map<Identifier, char[]> configs = new HashMap<>();
+    protected final Map<Identifier, char[][]> configs = new HashMap<>();
 
     @Override
     public <P> SimpleSideConfigComponent of(P provider) {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    @Override
+    public void writeNbt(NbtCompound tag) {
+        for (Entry<Identifier,char[][]> entry : configs.entrySet()) if (!DEFAULT_CHARS.equals(entry.getValue())) tag.putString(entry.getKey().toString(), SideConfigComponent.configArrayToString(entry.getValue()));
+    }
+
+    @Override
+    public void readNbt(NbtCompound tag) {
+        for (Identifier id : configs.keySet()) {
+            if (!tag.contains(id.toString(), NbtType.STRING)) continue;
+            configs.put(id, SideConfigComponent.configStringToArray(tag.getString(id.toString())));
+        }
+    }
+
+    @Override
+    public char getState(Identifier id, Direction dir, SideConfigBehavior behavior) {
+        return configs.get(id)[dir.ordinal()][behavior.ordinal()];
+    }
+
+    @Override
+    public void setState(Identifier id, Direction dir, SideConfigBehavior behavior, char state) {
+        configs.get(id)[dir.ordinal()][behavior.ordinal()] = state;
+    }
+
+    @Override
+    public void addSideConfig(Identifier id) {
+        configs.put(id, SideConfigComponent.getDefault());
+    }
+
+    @Override
+    public void removeSideConfig(Identifier id) {
+        configs.remove(id);
+    }
+
+    @Override
+    public void setAvailability(Identifier id, Direction dir, SideConfigBehavior behavior) {
+        setState(id, dir, behavior, getState(id, dir, behavior));
+    }
+
+    @Override
+    public boolean isAvailable(Identifier id, Direction dir, SideConfigBehavior behavior) {
+        return Character.isUpperCase(getState(id, dir, behavior));
+    }
+
+    //XXX: can this be done better?
+    @Override
+    public void change(Identifier id, Direction dir, SideConfigBehavior behavior) {
+        char c = getState(id, dir, behavior);
+        boolean active = Character.isUpperCase(c);
+        if (Character.toLowerCase(c) == 'f') c = 't';
+        else c = 'f';
+        if (active) Character.toUpperCase(c);
+        setState(id, dir, behavior, c);
+    }
+
+    @Override
+    public Set<Identifier> getIds() {
+        return configs.keySet();
     }
 }
